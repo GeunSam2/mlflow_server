04fe73a64fe4a111ad492285af25929fe8119e50
GeunSam2 <rootiron96@gmail.com>
2020-06-26 01:41:07 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 /usr/sbin/sshd
 mkdir -p /data2
-mlflow server --host 0.0.0.0 --default-artifact-root file:///data1 --backend-store-uri mysql+pymysql://root:kbsys1234@192.168.0.246:8083/yeom
+mlflow server --host 0.0.0.0 --default-artifact-root file:///data1 --backend-store-uri mysql+pymysql://<username>:<password>@<host>:<port>/<database>
